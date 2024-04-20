7419791ad160ccd0e77e557fa76981bef2c01186
Christian Finker <finker@akaryon.com>
2024-04-20 14:12:01 +0200

    add start for restful part

--- server.js
+++ server.js
@@ -6,6 +6,46 @@ const resolvers = require('./resolvers');
 const CodeSnippet = require('./models/codeSnippet');
 const Tag = require('./models/tag');
 
+const app = express();
+
+// RESTful API endpoints
+app.use(express.json());
+
+// CodeSnippet endpoints
+app.get('/api/code-snippets', async (req, res) => {
+    const { tag } = req.query;
+    try {
+        const codeSnippets = await CodeSnippet.all();
+        if (tag) {
+            const filteredSnippets = await Promise.all(codeSnippets.map(async snippet => {
+                const tags = await Tag.allByCodeSnippetId(snippet.id);
+                snippet.tags = tags;
+                return snippet;
+            }));
+            const filteredByTag = filteredSnippets.filter(snippet => snippet.tags.some(t => t.name === tag));
+            res.json(filteredByTag);
+        } else {
+            res.json(codeSnippets);
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// TODO Add other CRUD endpoints for CodeSnippet (POST, PUT, DELETE)
+
+// Tag endpoints
+app.get('/api/tags', async (req, res) => {
+    try {
+        const tags = await Tag.all();
+        res.json(tags);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// TODO Add other CRUD endpoints for Tag (POST, PUT, DELETE)
+
 // Create tables
 CodeSnippet.createTable();
 Tag.createTable();
